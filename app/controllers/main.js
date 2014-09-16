42495b1a82c72e7ee17f90210b7c0eab7be4572d
Mohsen Azimi <mazimi@apigee.com>
2014-09-17 02:16:37 +0300

    Move api to it's own controller

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -1,79 +1,10 @@
-var mongoose = require('mongoose');
-var Bin = mongoose.model('Bin');
-
-// Make a new Bin
-exports.create = function (req, res, next) {
-  Bin.create(req.body, function (err, result){
-    if (!err) {
-      res.cookie(result.getSecret(), true, {
-        maxAge: 900000, httpOnly: true
-      });
-      res.send(result.toBin());
-    }
-    next();
-  });
+// Render empty editor
+exports.new = function(req, res, next) {
+  next();
 };
 
-// Get latest version
-exports.latest = function (req, res, next) {
-  Bin.findById(req.params.id, function (err, result) {
-    if (!err) {
-      result.versions = result.versions.splice(-1);
-      res.send(result.toBin());
-    }
-    next();
-  });
+// Render editor with a bin
+exports.show = function(req, res, next) {
+  next();
 };
 
-// Update latest version of a bin
-exports.updateLastVersion = function (req, res, next) {
-  Bin.findById(req.params.id, function (err, result) {
-    if (!err) {
-      // If they have the cookie
-      if (req.cookies[result.getSecret()]) {
-        result.updateLastVersion(req.body, function (err){
-          if (!err) {
-            res.send(result.toBin());
-          }
-          next();
-        });
-      } else {
-        res.status(403).end();
-      }
-    }
-  });
-};
-
-// Add a new version
-exports.addVersion = function (req, res, next) {
-  Bin.findById(req.params.id, function (err, result) {
-    if (!err && result) {
-
-      // If they have the secret cookie, create a new version on top
-      if (req.cookies[result.getSecret()]) {
-        result.addVersion(req.body, function (err){
-          if (!err) {
-            res.send(result.toBin());
-          }
-          next();
-        });
-
-      // if they don't have the secret cookie, make a new bin
-      } else {
-        exports.create(req, res, next);
-      }
-    } else {
-      res.status(500, err).end();
-    }
-  });
-};
-
-// Get a specific version of a bin
-exports.getVersion = function (req, res, next) {
-  Bin.findById(req.params.id, function (err, result) {
-    if (!err) {
-      res.send(result.toBin(+req.params.version));
-    }
-    next();
-  });
-};
--- app/router.js
+++ app/router.js
@@ -1,11 +1,17 @@
-var main = require('./controllers/main');
+var api = require('./controllers/api');
+var api = require('./controllers/main');
 
 module.exports = function (app) {
 
-  app.post('/', main.create); // Make a new Bin
-  app.get('/:id', main.latest); // Get latest version
-  app.put('/:id', main.updateLastVersion); // Update latest version of a bin
-  app.post('/:id', main.addVersion); // Add a new version to a bin
-  app.get('/:id/:version', main.getVersion); // Get a specific version of a bin
+  // API endpoints
+  app.post('/api/', api.create); // Make a new Bin
+  app.get('/api/:id', api.latest); // Get latest version
+  app.put('/api/:id', api.updateLastVersion); // Update latest version of a bin
+  app.post('/api/:id', api.addVersion); // Add a new version to a bin
+  app.get('/api/:id/:version', api.getVersion); // Get a specific version of a bin
 
+  // Main endpoints
+  app.get('/', main.new); // Empty editor
+  app.get('/:id', main.show); // Editor loaded with a bin
+  app.get('/:id/:version', main.show); // Editor loaded with an specific version of a bin
 };
